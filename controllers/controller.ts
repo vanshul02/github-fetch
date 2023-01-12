197e4c2d5712811a3bf4db6f8d3e6869fd0b91d4
Vanshul <vanshul.ahuja@gmail.com>
2023-01-12 12:49:44 +0530

    Modified getUser for Testing Frontend

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -27,23 +27,38 @@ export const getRepos = async (req: Request, res: Response) => {
 
 export const getUser = async (req: Request, res: Response) => {
   const userName = req.params.userName;
+  const user: RootUserObject = {
+    login: 'vanshul02',
+    id: 89513486,
+    avatar_url: 'https://avatars.githubusercontent.com/u/89513486?v=4',
+    url: 'https://api.github.com/users/vanshul02',
+    html_url: 'https://github.com/vanshul02',
+    name: 'Vanshul Ahuja',
+    location: 'Jaipur, Rajasthan',
+    bio: 'Extremely motivated and a Passionate Developer',
+    email: '',
+    twitter_username: 'vanshul_ahuja',
+  };
   console.log('CONTROLLER getUser INFO: ', userName);
-  await axios
-    .get(`https://api.github.com/users/${userName}`)
-    .then(result => {
-      const userData: RootUserObject = result.data;
-      console.log('CONTROLLER getUser INFO: ', userData);
-      return res.status(200).json(userData);
-    })
-    .catch((err: AxiosError | Error) => {
-      console.log('CONTROLLER getUser ERR: ', err);
-      if (axios.isAxiosError(err)) {
-        return res
-          .status(err.status !== undefined ? err.status : 500)
-          .json(err.message);
-      } else {
-        // Just a stock error
-        return res.status(500).json(err.message);
-      }
-    });
+  setTimeout(() => {
+    return res.status(200).json(user);
+  }, 3000);
+  // await axios
+  //   .get(`https://api.github.com/users/${userName}`)
+  //   .then(result => {
+  //     const userData: RootUserObject = result.data;
+  //     console.log('CONTROLLER getUser INFO: ', userData);
+  //     return res.status(200).json(userData);
+  //   })
+  //   .catch((err: AxiosError | Error) => {
+  //     console.log('CONTROLLER getUser ERR: ', err);
+  //     if (axios.isAxiosError(err)) {
+  //       return res
+  //         .status(err.status !== undefined ? err.status : 500)
+  //         .json(err.message);
+  //     } else {
+  //       // Just a stock error
+  //       return res.status(500).json(err.message);
+  //     }
+  //   });
 };
