bf5600cecc47d85ce13cbddda5c46bc9ff38f32d
Vanshul <vanshul.ahuja@gmail.com>
2023-01-13 00:28:52 +0530

    Added Fake Data For Tesing Frontend

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -5,24 +5,93 @@ import axios, {AxiosError} from 'axios';
 export const getRepos = async (req: Request, res: Response) => {
   const userName = req.params.userName;
   console.log('CONTROLLER getRepo INFO: ', userName);
-  await axios
-    .get(`https://api.github.com/users/${userName}/repos`)
-    .then(result => {
-      const repoData: RootRepoObject[] = result.data;
-      console.log('CONTROLLER getRepo INFO: ', repoData);
-      return res.status(200).json(repoData);
-    })
-    .catch((err: AxiosError | Error) => {
-      console.log('CONTROLLER getRepo ERR: ', err);
-      if (axios.isAxiosError(err)) {
-        return res
-          .status(err.status !== undefined ? err.status : 500)
-          .json(err.message);
-      } else {
-        // Just a stock error
-        return res.status(500).json(err.message);
-      }
-    });
+  const repos: RootRepoObject[] = [
+    {
+      id: 586496200,
+      node_id: 'R_kgDOIvU4yA',
+      name: 'cdk-s3-to-rds',
+      full_name: 'vanshul02/cdk-s3-to-rds',
+      private: false,
+      html_url: 'https://github.com/vanshul02/cdk-s3-to-rds',
+      description:
+        'Using AWS CDK for cloud deployment of Lambda Function, S3 Bucket and RDS Aurora MySQL DB Instance',
+      fork: false,
+      url: 'https://api.github.com/repos/vanshul02/cdk-s3-to-rds',
+      topics: ['aws-cdk', 'aws-s3', 'javascript', 'nodejs', 'typescript'],
+      git_url: '',
+      size: 82,
+      language: 'TypeScript',
+    },
+    {
+      id: 586496200,
+      node_id: 'R_kgDOIvU4yA',
+      name: 'cdk-s3-to-rds',
+      full_name: 'vanshul02/cdk-s3-to-rds',
+      private: false,
+      html_url: 'https://github.com/vanshul02/cdk-s3-to-rds',
+      description:
+        'Using AWS CDK for cloud deployment of Lambda Function, S3 Bucket and RDS Aurora MySQL DB Instance',
+      fork: false,
+      url: 'https://api.github.com/repos/vanshul02/cdk-s3-to-rds',
+      topics: ['aws-cdk', 'aws-s3', 'javascript', 'nodejs', 'typescript'],
+      git_url: '',
+      size: 82,
+      language: 'TypeScript',
+    },
+    {
+      id: 586496200,
+      node_id: 'R_kgDOIvU4yA',
+      name: 'cdk-s3-to-rds',
+      full_name: 'vanshul02/cdk-s3-to-rds',
+      private: false,
+      html_url: 'https://github.com/vanshul02/cdk-s3-to-rds',
+      description:
+        'Using AWS CDK for cloud deployment of Lambda Function, S3 Bucket and RDS Aurora MySQL DB Instance',
+      fork: false,
+      url: 'https://api.github.com/repos/vanshul02/cdk-s3-to-rds',
+      topics: ['aws-cdk', 'aws-s3', 'javascript', 'nodejs', 'typescript'],
+      git_url: '',
+      size: 82,
+      language: 'TypeScript',
+    },
+    {
+      id: 586496200,
+      node_id: 'R_kgDOIvU4yA',
+      name: 'cdk-s3-to-rds',
+      full_name: 'vanshul02/cdk-s3-to-rds',
+      private: false,
+      html_url: 'https://github.com/vanshul02/cdk-s3-to-rds',
+      description:
+        'Using AWS CDK for cloud deployment of Lambda Function, S3 Bucket and RDS Aurora MySQL DB Instance',
+      fork: false,
+      url: 'https://api.github.com/repos/vanshul02/cdk-s3-to-rds',
+      topics: ['aws-cdk', 'aws-s3', 'javascript', 'nodejs', 'typescript'],
+      git_url: '',
+      size: 82,
+      language: 'TypeScript',
+    },
+  ];
+  setTimeout(() => {
+    return res.status(200).json(repos);
+  }, 3000);
+  // await axios
+  //   .get(`https://api.github.com/users/${userName}/repos`)
+  //   .then(result => {
+  //     const repoData: RootRepoObject[] = result.data;
+  //     console.log('CONTROLLER getRepo INFO: ', repoData);
+  //     return res.status(200).json(repoData);
+  //   })
+  //   .catch((err: AxiosError | Error) => {
+  //     console.log('CONTROLLER getRepo ERR: ', err);
+  //     if (axios.isAxiosError(err)) {
+  //       return res
+  //         .status(err.status !== undefined ? err.status : 500)
+  //         .json(err.message);
+  //     } else {
+  //       // Just a stock error
+  //       return res.status(500).json(err.message);
+  //     }
+  //   });
 };
 
 export const getUser = async (req: Request, res: Response) => {
--- interfaces/interface.ts
+++ interfaces/interface.ts
@@ -1,27 +1,13 @@
-export interface Owner {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  url: string;
-  type: string;
-  site_admin: boolean;
-}
-
 export interface RootRepoObject {
   id: number;
   node_id: string;
   name: string;
   full_name: string;
   private: boolean;
-  owner: Owner;
   html_url: string;
   description?: string;
   fork: boolean;
   url: string;
-  created_at: Date;
-  updated_at: Date;
-  pushed_at: Date;
   git_url: string;
   size: number;
   language: string;
